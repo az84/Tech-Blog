8a265a0b3268df6beb5361692af8792d1dfa04a0
azaidi <zaidialina1@gmail.com>
2021-10-09 10:30:11 -0400

    changes and updates

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+
+    console.log("******hit",req.body);
+    const newUser = await User.create({
+      username: req.body.name,
+      password: req.body.password,
+    });
+
+    console.log("****",newUser);
+
+    console.log("*********", req.session);
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.name;
+      req.session.loggedIn = true;
+
+      res.json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.name,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: 'No user account found!' });
+      return;
+    }
+
+    const validPassword = user.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'No user account found!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = user.id;
+      req.session.username = user.name;
+      req.session.loggedIn = true;
+
+      res.json({ user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json({ message: 'No user account found!' });
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
